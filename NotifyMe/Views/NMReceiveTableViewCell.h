c47d526e3cbbce3169e7b44f4e2cbb1e28f2c4d3
Jeff Kibuule <jeff.kibuule@outlook.com>
2014-08-14 15:59:26 -0500

    Add receive table cell view

---
+++ NotifyMe/Views/NMReceiveTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  NMReceiveTableViewCell.h
+//  NotifyMe
+//
+//  Created by Jeff Kibuule on 8/14/14.
+//  Copyright (c) 2014 ChaiOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NMReceiveTableViewCell : UITableViewCell
+
+@end
