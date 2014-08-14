fc0ee6d167b03890b7a9840fb75a071018091d29
Jeff Kibuule <jeff.kibuule@outlook.com>
2014-08-13 19:48:17 -0500

    Create constants file

---
+++ NotifyMe/NMConstants.h
@@ -0,0 +1,14 @@
+//
+//  NMConstants.h
+//  NotifyMe
+//
+//  Created by Jeff Kibuule on 8/13/14.
+//  Copyright (c) 2014 ChaiOne. All rights reserved.
+//
+
+#ifndef NotifyMe_NMConstants_h
+#define NotifyMe_NMConstants_h
+
+NSString * const NMDeviceTag = @"device-tag";
+
+#endif
